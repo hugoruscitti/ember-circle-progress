18da0a22bda96f825e03060d1063add2b8767d21
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-03-23 21:38:20 -0300

    Agregando archivo js desde vendor y poniendo un nombre al blueprint.

--- blueprints/ember-circle-progress/index.js
+++ blueprints/ember-circle-progress/index.js
@@ -1,4 +1,11 @@
 /* eslint-env node */
 module.exports = {
-  description: "ember-circle-progress"
+  description: "ember-circle-progress",
+  name: "ember-circle-progress",
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/circle-progress.js');
+  }
 };
