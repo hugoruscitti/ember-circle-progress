446fbbd7ad71e5f7a07887184e93d0374dbf6639
Hugo Ruscitti <hugoruscitti@gmail.com>
2018-03-24 12:51:17 -0300

    Realizando arreglo menor.

--- blueprints/ember-circle-progress/index.js
+++ blueprints/ember-circle-progress/index.js
@@ -1,11 +1,5 @@
 /* eslint-env node */
 module.exports = {
   description: "ember-circle-progress",
-  name: "ember-circle-progress",
-  normalizeEntityName: function() {},
-  included(app) {
-    this._super.included.apply(this, arguments);
-
-    app.import('vendor/circle-progress.js');
-  }
+  normalizeEntityName: function() {}
 };
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-circle-progress'
+  name: 'ember-circle-progress',
+  included(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import('vendor/circle-progress.js');
+  }
 };
